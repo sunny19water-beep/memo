e8572851d13bc235cad7902a318b9fb172aa6158
sunny19water-beep <sunny19water@icloud.com>
2026-08-07 14:04:42 +0900

    Headerコンポーネント作成

---
+++ laravel/resources/js/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DocumentSvg from './svgs/DocumentSvg.vue'
+</script>
+
+<template>
+    <header>
+        <div class="title">
+            <DocumentSvg/><h1 class="co">かんたんメモ</h1>
+        </div>
+        <p>思いついたことをサッと記録しよう</p>
+    </header>
+</template>
+
+<style scoped>
+.title{
+    color: orange;
+    display: flex;
+    justify-content: center;
+}
+header{
+    text-align: center;
+}
+</style>
